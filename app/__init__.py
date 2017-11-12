3ca87e7a0056c156470e04dbed3013ffc5923e5e
Charlie Root <fkovaldev@gmail.com>
2017-11-12 10:38:45 +0100

    Adding LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@
 # third-party imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 # local imports
 from config import app_config
 
 # db variable initialization
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app(config_name):
     print("XXX config_name:{0}".format(config_name))
