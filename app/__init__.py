edfbcab119e2ecaec00aa0d1cdc2bdb9750c46d5
Charlie Root <fkovaldev@gmail.com>
2017-11-13 00:05:18 +0100

    Part I - complete

--- app/__init__.py
+++ app/__init__.py
@@ -2,34 +2,30 @@
 
 # third-party imports
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 # local imports
 from config import app_config
 
-# db variable initialization
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app(config_name):
     print("XXX config_name:{0}".format(config_name))
     print("XXX __name__:{0}".format(__name__))
+
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+
+    Bootstrap(app)
     db.init_app(app)
-
-    # temporary route
-#    @app.route('/')
-#    def hello_world():
-#        return 'Hello, World!'
-
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
-
     migrate = Migrate(app, db)
 
     from app import models
